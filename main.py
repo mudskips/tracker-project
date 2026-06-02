d65975f901914f6148b77adbc060e1ecee284db6
Jack Grady <275314523+mudskips@users.noreply.github.com>
2026-06-02 17:51:32 -0400

    Create landmark tracker for two hands

--- main.py
+++ main.py
@@ -4,7 +4,26 @@ import mediapipe as mp
 Base_options = mp.tasks.BaseOptions
 Hand_detector =mp.tasks.vision.HandLandmarker
 Hand_detector_options = mp.tasks.vision.HandLandmarkerOptions
-options = Hand_detector_options(Base_options(model_asset_path="hand_landmarker.task"))
+options = Hand_detector_options(Base_options(model_asset_path="hand_landmarker.task"), num_hands=2)
 
 detector = Hand_detector.create_from_options(options)
-print(f"model loaded successfully! nice.")
+print(f"loaded. connecting to default camera")
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    result, frame = cap.read()
+    frame = cv2.flip(frame, 1)
+    rgb_corrected = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    mp_ready_frame = mp.Image(image_format= mp.ImageFormat.SRGB, data=rgb_corrected)
+    detection = detector.detect(mp_ready_frame)
+    if detection.hand_landmarks:
+        for hand in detection.hand_landmarks:
+            for landmark in hand:
+                x = int(landmark.x * frame.shape[1])
+                y = int(landmark.y * frame.shape[0])
+                cv2.circle(frame, (x,y), 6, (0, 30, 0), -1)
+    cv2.imshow("P*lantir drone (press space to close)", frame)
+    if cv2.waitKey(1) & 0xFF == ord(" "):
+        break
+
