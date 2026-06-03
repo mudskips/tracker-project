468e2ede37fa3483ef93daf338830a48966f8bc7
Jack Grady <275314523+mudskips@users.noreply.github.com>
2026-06-02 22:42:48 -0400

    Begin dev data collection

--- datacollect.py
+++ datacollect.py
@@ -14,3 +14,19 @@ cap = cv2.VideoCapture(0)
 recording = False
 sign_name = "infinite void"
 target_file = f"{sign_name}.csv"
+
+while True:
+    result, frame = cap.read()
+    frame = cv2.flip(frame, 1)
+    rgb_corrected = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    mp_ready_frame = mp.Image(image_format= mp.ImageFormat.SRGB, data=rgb_corrected)
+    detection = detector.detect(mp_ready_frame)
+    if detection.hand_landmarks:
+        data = []
+        for hand in detection.hand_landmarks:
+            for landmark in hand:
+                data.append(landmark.x)
+                data.append(landmark.y)
+        while len(data) < 84:
+            row.append(0.0)
+        
