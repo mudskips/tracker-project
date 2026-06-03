b2dd451145de241e77ed552c9bc44b2cec94eaa6
Jack Grady <275314523+mudskips@users.noreply.github.com>
2026-06-03 10:59:31 -0400

    Train model on my data, get 99% accuracy, be a beast

--- train.py
+++ train.py
@@ -3,5 +3,19 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import pandas as pd
 
-in_vd = pd.read_csv("infinite void.csv")
-natural
+in_vd = pd.read_csv("infinite void.csv", header = None)
+natural = pd.read_csv("natural.csv", header = None)
+
+ovr_data = pd.concat([in_vd, natural])
+nums = ovr_data.iloc[:,1:]
+name = ovr_data.iloc[:,0]
+
+nums_train, nums_test, name_train, name_test = train_test_split(nums, name, test_size=0.3, random_state=13)
+
+my_model = RandomForestClassifier(n_estimators=150, random_state=13)
+my_model.fit(nums_train, name_train)
+print(f"model training complete :)")
+model_predictions= my_model.predict(nums_test)
+print(accuracy_score(name_test, model_predictions))
+
+
