f50f4cb890e84b9e6f8a22afc21f66ef8ea7c200
Jack Grady <275314523+mudskips@users.noreply.github.com>
2026-06-03 11:14:52 -0400

    Create and save trained prediction model

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import pandas as pd
+import joblib
 
 in_vd = pd.read_csv("infinite void.csv", header = None)
 natural = pd.read_csv("natural.csv", header = None)
@@ -10,12 +11,15 @@ ovr_data = pd.concat([in_vd, natural])
 nums = ovr_data.iloc[:,1:]
 name = ovr_data.iloc[:,0]
 
-nums_train, nums_test, name_train, name_test = train_test_split(nums, name, test_size=0.3, random_state=13)
+nums_train, nums_test, name_train, name_test = train_test_split(nums, name, test_size=0.25, random_state=13)
 
 my_model = RandomForestClassifier(n_estimators=150, random_state=13)
 my_model.fit(nums_train, name_train)
-print(f"model training complete :)")
 model_predictions= my_model.predict(nums_test)
-print(accuracy_score(name_test, model_predictions))
+print(f"accuracy: {accuracy_score(name_test, model_predictions)}")
 
+#0.1 test size gave 1.0 accuracy, seemed off
+#0.3 test size gave 0.99874 accuracy, verifies model accuracy
 
+joblib.dump(my_model, "domain_model.pkl")
+print("model has been saved")
