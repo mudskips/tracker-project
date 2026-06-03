58fbd5db6cfc142a65af4b7e3145088ad30db88b
Jack Grady <275314523+mudskips@users.noreply.github.com>
2026-06-03 11:54:34 -0400

    Add precidtions to main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import cv2
 import mediapipe as mp
+import joblib
+import numpy as np
+my_model = joblib.load("domain_model.pkl")
 
 Base_options = mp.tasks.BaseOptions
 Hand_detector =mp.tasks.vision.HandLandmarker
@@ -18,11 +21,17 @@ while True:
     mp_ready_frame = mp.Image(image_format= mp.ImageFormat.SRGB, data=rgb_corrected)
     detection = detector.detect(mp_ready_frame)
     if detection.hand_landmarks:
-        for hand in detection.hand_landmarks:
-            for landmark in hand:
-                x = int(landmark.x * frame.shape[1])
-                y = int(landmark.y * frame.shape[0])
-                cv2.circle(frame, (x,y), 6, (0, 30, 0), -1)
+        data = []
+        wrist_y = detection.hand_landmarks[0][0].y
+        wrist_x = detection.hand_landmarks[0][0].x
+        for landmark in detection.hand_landmarks[0]:
+            data.append(landmark.x - wrist_x)
+            data.append(landmark.y - wrist_y)
+            x = int(landmark.x * frame.shape[1])
+            y = int(landmark.y * frame.shape[0])
+            cv2.circle(frame, (x, y), 6, (0, 30, 0), -1)
+        model_prediction= my_model.predict([data])[0]
+        print(model_prediction)
     cv2.imshow("P*lantir drone (press space to close)", frame)
     if cv2.waitKey(1) & 0xFF == ord(" "):
         break
